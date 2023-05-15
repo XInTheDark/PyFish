59c1bcdef416570c66e4562611abe94684631f38
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-05-15 12:31:24 +0800

    Add build.sh

---
+++ src/build.sh
@@ -0,0 +1,13 @@
+pyinstaller main.py --onefile -n pyfish
+
+# Path: src/dist/pyfish
+
+# Delete the build folder and pyfish.spec
+rm -rf build
+rm pyfish.spec
+
+# Move the pyfish executable to main folder
+mv dist/pyfish ../pyfish
+
+# Delete the dist folder
+rm -rf dist
